8c8c2457009ab5fbd32de629b9ae8771b46f224d
leneti <d.gudauskas1@gmail.com>
2022-02-23 21:47:40 +0000

    Added initial Stepper and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { Box, Stepper, Button, Group } from "@mantine/core";
 
 function App() {
+  const [active, setActive] = useState(0);
+  const nextStep = () =>
+    setActive((current) => (current < 3 ? current + 1 : current));
+  const prevStep = () =>
+    setActive((current) => (current > 0 ? current - 1 : current));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Box style={{ width: "75%" }}>
+        <Stepper
+          color="yellow"
+          active={active}
+          onStepClick={setActive}
+          breakpoint="sm"
         >
-          Learn React
-        </a>
-      </header>
+          <Stepper.Step
+            label="Fist step"
+            description="Upload bill picture"
+            allowStepSelect={active > 0}
+          >
+            Upload a picture of the bill
+          </Stepper.Step>
+          <Stepper.Step
+            label="Second step"
+            description="Image parsing"
+            allowStepSelect={active > 1}
+          >
+            Wait for Google Vision API to finish parsing the image
+          </Stepper.Step>
+          <Stepper.Step
+            label="Final step"
+            description="Separate items"
+            allowStepSelect={active > 2}
+          >
+            Separate items from the common list
+          </Stepper.Step>
+          <Stepper.Completed>
+            Visit Splitwise to settle expenses
+          </Stepper.Completed>
+        </Stepper>
+
+        <Group position="center" mt="xl">
+          <Button variant="default" onClick={prevStep}>
+            Back
+          </Button>
+          <Button color="yellow" onClick={nextStep}>
+            {active > 1 ? "Finish" : "Next step"}
+          </Button>
+        </Group>
+      </Box>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { MantineProvider } from "@mantine/core";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider
+      theme={{
+        colorScheme: "dark",
+        colors: {
+          primary: [
+            "#fffbeb",
+            "#fef3c7",
+            "#fde68a",
+            "#fcd34d",
+            "#fbbf24",
+            "#f59e0b",
+            "#d97706",
+            "#b45309",
+            "#92400e",
+            "#78350f",
+          ],
+          background: ["#404040", "#4a4a4a", "#363636", "#262626", "#595959"],
+          backgroundLight: [
+            "#F6F6F6",
+            "#FFF",
+            "#ECECEC",
+            "#DCDCDC",
+            "#FFF",
+            "#6F6F6F",
+          ],
+        },
+      }}
+    >
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
